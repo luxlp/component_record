d418f7acc614fea9b490468c1c4a51a5c684e46c
luis <luigi836@gmail.com>
2022-01-21 12:02:08 -0500

    Update component_tracker.py

--- component_tracker.py
+++ component_tracker.py
@@ -16,8 +16,10 @@ computer_user = getuser()
 data_dir = fr'C:\Users\luis.peguero\Desktop\Eng_\humboldt16_component_tracker_'
 #csv_file
 csv_file = f'{data_dir}./humboldt16_component_tracker.csv'
+#Github url
+url = 'https://github.com/luxlp/component_record/blob/main/humboldt16_component_tracker.csv?raw=true'
+t_url = 'https://github.com/luxlp/component_record/blob/main/in_door.csv?raw=true'
 #date
-today_ = date.today().strftime('%m/%d/%y')
 def time_conversion():
     utc_now = pytz.utc.localize(datetime.utcnow())
     est = utc_now.astimezone(pytz.timezone('US/Eastern'))
@@ -27,8 +29,8 @@ eastern_time_ = time_conversion()
 
 #Page-Setup-----------------------------------------------------------------------------------------------------------
 st.set_page_config(
-    page_title='Humboldt16 Component Tracker',
-    page_icon=':Jack-O-Lantern:',
+    page_title='Component Tracker',
+    page_icon=':computer:',
     layout='wide',
     initial_sidebar_state='auto'
 )
@@ -41,10 +43,63 @@ def load_df():
 df_ = load_df()
 
 #Main title of page
-st.title('Humboldt16 Component Tracker :computer:')
+st.title('H16 Component Tracker :computer:')
 
 #set columns
 first_column, second_column = st.columns(2)
+
+#Github Process
+dataunlock = pd.read_csv(t_url, on_bad_lines='skip')
+dfl = pd.DataFrame(dataunlock)
+
+p_one = dfl.iloc[0,0]
+p_two = dfl.iloc[1,0]
+p_three = dfl.iloc[2,0]
+unite = str(p_one + p_two + p_three)
+
+df2_ = df_.to_csv(sep=',', index=False)
+
+file_list = [df2_]
+file_name = ['component_tracker.csv']
+
+commit_message = 'test python'
+
+#github connection
+user = 'luxlp'
+password = unite
+git = Github(user, password)
+
+#connect to repo
+repo = git.get_user('luxlp').get_repo('componenet_record')
+#check file in repo
+x = repo.get_contents('component_tracker.csv')
+
+#getbranches
+x = repo.get_git_refs()
+for y in x:
+    print(y)
+
+#ref
+master_ref = repo.get_git_ref("heads/main")
+
+def updategitfile(file_name, file_list, userid, pwd, Repo, commit_message = ''):
+    if commit_message == '':
+        commit_message = 'Data Updated - ' + eastern_time_
+        
+        git = Github(userid,pwd)
+        repo = git.get_user().get_repo(Repo)
+        master_ref = repo.get_git_ref('heads/main')
+        master_sha = master_ref.object.sha
+        base_tree = repo.get_git_tree(master_sha)
+        element_list = list()
+        for i in range(0, len(file_list)):
+            element = InputGitTreeElement(file_name[i], '100644', 'blob', file_list[i])
+            element_list.append(element)
+        tree = repo.create_git_tree(element_list, base_tree)
+        parent = repo.get_git_commit(master_sha)
+        commit = repo.create_git_commit(commit_message, tree, [parent])
+        master_ref.edit(commit.sha)
+        print('Update complete')
 
 #set first column
 with first_column:
@@ -85,7 +140,9 @@ with first_column:
             if submitted:
                 try:
                     df_ = df_.append(dfin, ignore_index= True)
-                    df_.to_csv(csv_file, index = False)
+                    df2_ = df_.to_csv(sep=',', index=False)
+                    file_list = [df2_]
+                    updategitfile(file_name, file_list, user, password, 'componenet_record', 'heads/main') 
                 except:
                     pass
                     
@@ -102,3 +159,8 @@ with second_column:
         file_name = f'{today_}_humboldt16_component_tracker.csv',
         mime = 'text/csv'
     )
+    
+
+        
+        
+        
